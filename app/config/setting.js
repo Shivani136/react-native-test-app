4bfa32796f6d2007914514e9bafb2e669e9cd66c
Shivani136 <shivanichourasiya18@gmail.com>
2022-12-02 17:23:07 +0530

    make build of APK 2 Dec

--- app/config/setting.js
+++ app/config/setting.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "http://192.168.168.61:9000/api",
+    apiUrl: "https://demoreactnativeapp.herokuapp.com/api",
   },
   staging: {
-    apiUrl: "http://192.168.168.61:9000/api",
+    apiUrl: "https://demoreactnativeapp.herokuapp.com/api",
   },
   prod: {
-    apiUrl: "http://192.168.168.61:9000/api",
+    apiUrl: "https://demoreactnativeapp.herokuapp.com/api",
   },
 };
 
